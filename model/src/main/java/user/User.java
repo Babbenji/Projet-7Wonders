5dd263d48e2bf43984a1c3cb1b963a4dc4c1c63b
Julien Lietard <contact.julienlietard@gmail.com>
2022-01-03 15:19:27 +0100

    Implémentation partielle de User

--- model/src/main/java/user/User.java
+++ model/src/main/java/user/User.java
@@ -1,5 +1,8 @@
 package user;
 
+import interfaces.exceptions.JoueurDejaDansLaListeDAmisException;
+import interfaces.exceptions.JoueurNonExistantException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,26 +11,54 @@ public class User {
     private String pseudo;
     private List<User> amis;
 
+    /**
+     * Permet d'ajouter un utilisateur à sa liste d'amis
+     * @param pseudo
+     */
     public User(String pseudo) {
         this.pseudo = pseudo;
         this.amis = new ArrayList<>();
     }
 
-    public void addFriend(String pseudo){
-        this.amis.add(this.getUserByPseudo(pseudo));
+    /**
+     * Permet d'ajouter un utilisateur à sa liste d'amis
+     * @param pseudo
+     * @throws JoueurNonExistantException : Le joueur n'existe pas
+     * @throws JoueurDejaDansLaListeDAmisException : Le joueur est déjà dans la liste d'amis
+     */
+    public void addFriend(String pseudo) throws JoueurNonExistantException, JoueurDejaDansLaListeDAmisException {
+        for (User ami : this.amis) {
+            if(ami.getPseudo().equals(pseudo)){
+                throw new JoueurDejaDansLaListeDAmisException();
+            }
+            else {
+                this.amis.add(this.getUserByPseudo(pseudo)); //rajouter la condition du si le joueur existe quand la bdd sera faite
+            }
+        }
     }
 
-
-    public User getUserByPseudo(String pseudo){
+    /**
+     * Permet de retrouver un joueur via son pseudo
+     * @param pseudo
+     * @return
+     */
+    public User getUserByPseudo(String pseudo){ //Cette méthode ne sera pas dans la classe User !!!!!!
         return this;//requete mongodb
-
     }
 
+    /**
+     * Permet de retourner le pseudo de l'utilisateur
+     * @return pseudo
+     */
     public String getPseudo() {
-        return pseudo;
+        return this.pseudo;
     }
 
+    /**
+     * Permet de retourner la liste des amis
+     * @return amis
+     */
     public List<User> getAmis() {
-        return amis;
+        return this.amis;
     }
 }
