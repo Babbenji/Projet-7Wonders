c6689efa4ece64d55fbd817c7dad943768a1612e
Blond Matthieu <matthieu.blond@etu.univ-orleans.fr>
2022-01-08 19:06:09 +0100

    Vues MenuNonConnecte, Connexion, Inscription en fxml et leurs java. Adaptation du controleur et de Main.java. Rajout de sons à implémenter plus tard. Désormais, les vues citées permettent de naviguer entre elles et le bouton quitter du Menu ferme le programme.

--- clientJfx/src/main/java/controleur/Controleur.java
+++ clientJfx/src/main/java/controleur/Controleur.java
@@ -3,6 +3,8 @@ package controleur;
 import javafx.stage.Stage;
 import modele.ProxySevenWondersOnLine;
 import modele.ProxySevenWondersOnLineImpl;
+import vues.VueConnexion;
+import vues.VueInscription;
 import vues.VueMenuNonConnecte;
 
 import java.io.IOException;
@@ -11,15 +13,47 @@ public class Controleur
 {
     private ProxySevenWondersOnLine facade;
     private VueMenuNonConnecte vueMenuNonConnecte;
+    private VueConnexion vueConnexion;
+    private VueInscription vueInscription;
+
 
 
     public Controleur(Stage stage) throws IOException {
         facade = new ProxySevenWondersOnLineImpl();
+
         vueMenuNonConnecte = VueMenuNonConnecte.creerVue(stage);
+        vueMenuNonConnecte.initialiserControleur(this);
+        vueConnexion = VueConnexion.creerVue(stage);
+        vueConnexion.initialiserControleur(this);
+        vueInscription = VueInscription.creerVue(stage);
+        vueInscription.initialiserControleur(this);
+
     }
 
     public void run()
     {
         vueMenuNonConnecte.show();
     }
+
+    public void goToMenu() {
+        this.facade = new ProxySevenWondersOnLineImpl();
+        this.vueMenuNonConnecte.show();
+    }
+
+    public void goToConnexion() {
+        this.facade = new ProxySevenWondersOnLineImpl();
+        this.vueConnexion.show();
+    }
+
+    public void goToInscription() {
+        this.facade = new ProxySevenWondersOnLineImpl();
+        this.vueInscription.show();
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
 }
+
+
+
---
+++ clientJfx/src/main/java/vues/VueInscription.java
@@ -0,0 +1,113 @@
+package vues;
+
+import controleur.Controleur;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+public class VueInscription implements Vue{
+
+    /* Les noms des attributs doivent être les noms des fx:id */
+    @FXML
+    private AnchorPane baseAnchor;
+    @FXML
+    private ImageView imageFond;
+    @FXML
+    private ImageView imageLogo;
+    @FXML
+    private VBox vbChamps;
+    @FXML
+    private TextField textFieldPseudo;
+    @FXML
+    private PasswordField textFieldMDP;
+    @FXML
+    private Button buttonRetour;
+    @FXML
+    private Button buttonValider;
+
+    private Scene scene;
+    private Controleur controleur;
+    private Stage stage;
+
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    private void initialiserVue() {
+        this.scene = new Scene(baseAnchor, 1920, 1080);
+    }
+
+    public static VueInscription creerVue(Stage stage) throws IOException
+    {
+        URL location = VueInscription.class.getResource("inscription.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+        Parent root = null;
+        try {
+            root = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        VueInscription vue = fxmlLoader.getController();
+        vue.setStage(stage);
+        Scene scene = new Scene(root);
+        vue.setScene(scene);
+        vue.initBoutonRetour();
+        vue.initBoutonValider();
+        return vue;
+    }
+
+    private void initBoutonValider() {
+        this.buttonValider.setOnAction(e -> connexion());
+    }
+
+    private void initBoutonRetour() {
+        this.buttonRetour.setOnAction(e -> goMenu());
+    }
+
+    private void connexion() {
+        String pseudo = this.textFieldPseudo.getText();
+        if (Objects.isNull(pseudo) || pseudo.length()<2) {
+            Alert alert = new Alert(Alert.AlertType.ERROR,"Vous devez renseigner un pseudo d'au moins 2 caractères !!", ButtonType.OK);
+            alert.showAndWait();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"OK 👌", ButtonType.OK);
+            alert.showAndWait();
+            //controleur.connexion(pseudoJoueur);
+        }
+    }
+    private void goMenu() {
+        this.controleur.goToMenu();
+    }
+
+    @Override
+    public void show() {
+        this.stage.setScene(this.scene);
+        this.stage.show();
+    }
+
+    @Override
+    public void chargerDonnees() {
+
+    }
+
+    @Override
+    public void initialiserControleur(Controleur controleur) {
+        this.controleur = controleur;
+    }
+}
