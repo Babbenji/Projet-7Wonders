50217b2cb015e276d7083874ff205802b4cd93ea
Julien Lietard <contact.julienlietard@gmail.com>
2022-01-09 00:00:37 +0100

    ajout des messages d'erreurs ConnectBDExeption et PseudoDejaPrisException

--- dao/src/main/java/services/exceptions/ConnectBDExeption.java
+++ dao/src/main/java/services/exceptions/ConnectBDExeption.java
@@ -1,4 +1,8 @@
 package services.exceptions;
 
 public class ConnectBDExeption extends Exception {
+    @Override
+    public String toString() {
+        return "Erreur lors de la connexion à la base de données.";
+    }
 }
